ee9738f36ef7b1ce48019a6e76d30c3718778f9a
Kevin Labianco <klabianco@gmail.com>
2022-09-21 13:04:34 -0700

    add google analytics....

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-9PQ2Z2KZ4Y`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9PQ2Z2KZ4Y', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
