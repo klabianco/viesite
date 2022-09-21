8377b4005e038d59c33b5c1ea42a1b285d2443aa
Kevin Labianco <klabianco@gmail.com>
2022-09-21 09:51:37 -0700

    bugfix

--- components/FernNavbar.js
+++ components/FernNavbar.js
@@ -8,7 +8,7 @@ const FernNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
-      <Navbar.Brand href="/">VV's Faves</Navbar.Brand>
+      <Navbar.Brand href="/">VV&apos;s Faves</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
