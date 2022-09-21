875db29b94b28b13e3885a3cc0a3507e7633827f
Kevin Labianco <klabianco@gmail.com>
2022-09-21 09:49:14 -0700

    bugfix

--- components/FernNavbar.js
+++ components/FernNavbar.js
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const FernNavbar = () => {
 
   return (
-    <Navbar bg="dark" variant='dark' expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
       <Navbar.Brand href="/">VV's Faves</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
