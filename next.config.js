b1b969f39e96d2d54f67fa8b8d6134b7ce390df6
Kevin Labianco <klabianco@gmail.com>
2022-10-18 13:02:56 -0700

    adding sitemap

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const nextConfig = {
   swcMinify: true,
 }
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
+  },
+}
+
 module.exports = nextConfig
